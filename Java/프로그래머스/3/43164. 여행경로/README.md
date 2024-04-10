6e9d43b9ec58ad017905e15bcae8cf63a63386c0
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-04-10 19:46:36 +0900

    [level 3] Title: 여행경로, Time: 29.50 ms, Memory: 77.8 MB -BaekjoonHub

--- Java/프로그래머스/3/43164. 여행경로/README.md
+++ Java/프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 76.1 MB, 시간: 10.41 ms
+메모리: 77.8 MB, 시간: 29.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 19:16:32
+2024년 04월 10일 19:46:35
 
 ### 문제 설명
 
--- Java/프로그래머스/3/43164. 여행경로/여행경로.java
+++ Java/프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,34 +1,35 @@
 import java.util.*;
 
 class Solution {
-    boolean[] visited;
-    ArrayList<String> allRoute; 
+    List<String> routes; 
+    int[] visited;
     
     public String[] solution(String[][] tickets) {
         String[] answer = {};
-        int cnt = 0;
-        visited = new boolean[tickets.length];
-        allRoute = new ArrayList<>();
         
-        dfs("ICN", "ICN", tickets, cnt);
+        int visitedAirports = 0;
+        routes = new ArrayList<>();
+        visited = new int[tickets.length];
         
-        Collections.sort(allRoute);
-        answer = allRoute.get(0).split(" ");
+        dfs("ICN", "ICN", tickets, visitedAirports);
+        
+        Collections.sort(routes);
+        answer = routes.get(0).split(" ");
         
         return answer;
     }
     
-    public void dfs(String start, String route, String[][] tickets, int cnt){
-        if(cnt == tickets.length){
-            allRoute.add(route);
+    public void dfs(String start, String route, String[][] tickets, int visitedAirports){
+        if (visitedAirports == tickets.length){
+            routes.add(route);
             return;
         }
         
-        for(int i=0; i<tickets.length; i++){
-            if(start.equals(tickets[i][0]) && !visited[i]){
-                visited[i] = true;
-                dfs(tickets[i][1], route+" "+tickets[i][1], tickets, cnt+1);
-                visited[i] = false;
+        for(int i = 0; i < tickets.length; i++){
+            if(start.equals(tickets[i][0]) && visited[i] == 0){
+                visited[i] = 1;
+                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, visitedAirports + 1);
+                visited[i] = 0;
             }
         }
     }
