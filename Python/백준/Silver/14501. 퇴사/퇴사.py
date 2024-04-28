d3c608d15379570572bf267519481a67be17b684
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-04-28 09:38:43 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ Python/백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+t_arr = []
+p_arr = []
+for i in range(n):
+    t, p = map(int, input().split())
+    t_arr.append(t)
+    p_arr.append(p)
+
+dp = [0 for i in range(n+1)]
+for i in range(n):
+    for j in range(i + t_arr[i], n+1):
+        if dp[i] + p_arr[i] > dp[j]:
+            dp[j] = dp[i] + p_arr[i]
+
+print(dp[-1])
