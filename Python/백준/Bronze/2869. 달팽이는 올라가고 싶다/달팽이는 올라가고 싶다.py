bb9c3376cdde39c0fd6395336bd4183036abdd68
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-04-28 09:10:28 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -0,0 +1,13 @@
+a, b, v = map(int, input().split())
+
+distance = v - a
+days = distance // (a-b) + 1
+if a == v:
+    print(1)
+else:
+    if (distance <= (a-b)): #등호 있이?없이?
+        days += 1
+    if ((a-b)*(days-1)+a < v):
+        days += 1
+
+    print(days)
