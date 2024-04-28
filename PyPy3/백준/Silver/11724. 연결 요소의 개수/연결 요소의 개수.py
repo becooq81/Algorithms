a00106805d6ff6b514949f00db633fb2c1b03c2a
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-04-28 09:38:04 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 456 ms, Memory: 172872 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,31 @@
+def bfs(start):
+    visited[start] = 1
+    queue = [start]
+    count = 0
+    
+    while queue:
+        node = queue.pop(0)
+        
+        for i in graph[node]:
+            if visited[i] == 0:
+                visited[i] = 1
+                queue.append(i)
+                count += 1
+    return count
+
+N, M = map(int, input().split())
+N += 1
+graph = [list() for _ in range(N)]
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+visited = [0] * N
+ 
+cntcmpt = 0
+for i in range(1, N):
+    if visited[i] == 0:
+        tmp = bfs(i)
+        cntcmpt += 1
+            
+print(cntcmpt)
