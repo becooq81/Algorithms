d98680313921bab7a3b0eb1e53ed83d6fa53451d
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-06-17 11:28:47 +0900

    Delete step-by-step/기하: 직사각형과 삼각형 directory

--- step-by-step/기하: 직사각형과 삼각형/14125.py
+++
@@ -1,9 +0,0 @@
-a, b, c = map(int, input().split())
-
-set = [a, b, c]
-longest = max(set)
-set.remove(longest)
-if sum(set) <= longest:
-    print(sum(set) + sum(set)-1)
-else:
-    print(sum(set) +longest)
