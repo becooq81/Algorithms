eb0a27ba40782eea0609c2f95da12c735a6f20cc
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-01 15:32:08 +0900

    [Silver II] Title: 키로거, Time: 2484 ms, Memory: 147264 KB -BaekjoonHub

--- Java/백준/Silver/5397. 키로거/키로거.java
+++ Java/백준/Silver/5397. 키로거/키로거.java
@@ -17,35 +17,26 @@ public class Main {
 
     private static String solvePassword(char[] input) {
         LinkedList<Character> output = new LinkedList<>();
-        // StringBuilder output = new StringBuilder();
         int idx = 0;
         int outputIdx = 0;
         while (idx < input.length) {
             char current = input[idx];
             if (current == MOVE_LEFT) {
                 outputIdx = Math.max(0, outputIdx - 1);
-                // System.out.printf(" move left at: %d, outputIdx: %d\n", idx, outputIdx);
             } else if (current == MOVE_RIGHT) {
                 outputIdx = Math.min(output.size(), outputIdx + 1);
-                // System.out.printf(" move right at: %d, outputIdx: %d\n", idx, outputIdx);
             } else if (current == BACKSPACE) {
                 if (!output.isEmpty()) {
-                    // outputIdx = Math.min(output.size() - 1, outputIdx);
                     if (outputIdx != 0) {
                         output.remove(outputIdx - 1);
                         outputIdx = Math.max(0, --outputIdx);
                     }
-                    // System.out.printf(" popped at: %s\n", output.toString());
                 }
             } else {
                 output.add(outputIdx++, current);
                 outputIdx = Math.min(output.size(), outputIdx);
-                // System.out.printf(" pushed at: %s\n", output.toString());
             }
             idx++;
-            // System.out.printf("Output curr idx: %d, current size: %d\n", outputIdx,
-            // output.size());
-
         }
 
         StringBuilder sb = new StringBuilder();
