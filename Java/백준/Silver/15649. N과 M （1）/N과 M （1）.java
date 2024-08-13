08692d8c64f9f95f812243d4e90e68a249fa6e15
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-13 13:44:56 +0900

    [Silver III] Title: N과 M (1), Time: 304 ms, Memory: 33932 KB -BaekjoonHub

--- Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,44 +3,44 @@ import java.io.*;
 
 public class Main {
     static final char NEW_LINE = '\n', WHITESPACE = ' ';
+
     static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     static int[] visited;
     static int N, M;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         visited = new int[N+1];
 
-        makePermutation(new LinkedList<>());
+        makePermutation(0, new int[M]);
 
         System.out.append(sb);
     }
 
-    private static void makePermutation(LinkedList<Integer> tmp) {
-        if (tmp.size() == M) {
+    private static void makePermutation(int idx, int[] tmp) {
+        if (idx == M) {
             sb.append(listToString(tmp)).append(NEW_LINE);
             return;
         }
         for (int i = 1; i <= N; i++) {
             if (visited[i] == 0) {
-                tmp.addLast(i);
+                tmp[idx] = i;
                 visited[i] = 1;
-                makePermutation(tmp);
+                makePermutation(idx + 1, tmp);
                 visited[i] = 0;
-                tmp.removeLast();
+                tmp[idx] = 0;
             }
         }
     }
 
-    private static String listToString(LinkedList<Integer> tmp) {
+    private static String listToString(int[] tmp) {
         StringBuilder sb = new StringBuilder();
-        ListIterator<Integer> ll = tmp.listIterator();
-        while (ll.hasNext()) {
-            sb.append(ll.next()).append(WHITESPACE);
+        for (int a : tmp) {
+            sb.append(a).append(WHITESPACE);
         }
         return sb.toString().trim();
     }
