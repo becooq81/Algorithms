b02482da0360f0e20640286b57c7db3eba61155c
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-14 18:59:19 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 105 ms, Memory: 18,380 KB -BaekjoonHub

--- Java/SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ Java/SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -3,10 +3,11 @@ import java.io.*;
 
 class Solution {
 
-    static Map<Character, Character> pairs = new HashMap<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+        Map<Character, Character> pairs = new HashMap<>();
 
         pairs.put('[',']');
         pairs.put('<','>');
@@ -15,22 +16,24 @@ class Solution {
 
         for (int t = 0; t < 10; t++) {
             int n = Integer.parseInt(br.readLine());
-            boolean success = solve(br.readLine().toCharArray());
+            boolean success = solve(br.readLine().toCharArray(), pairs);
             sb.append("#").append(t+1).append(" ").append(success ? 1 : 0);
             if (t != 9) sb.append("\n");
         }
-        System.out.append(sb);
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        
     }
 
-    private static boolean solve(char[] parantheses) {
+    private static boolean solve(char[] parantheses, Map<Character, Character> pairs) {
         ArrayDeque<Character> stack = new ArrayDeque<>();
 
         for (char p : parantheses) {
-            if (!stack.isEmpty()) {
-                if (pairs.containsKey(stack.getLast()) && pairs.get(stack.getLast()) == p) {
+            if (!stack.isEmpty() && pairs.containsKey(stack.getLast()) && pairs.get(stack.getLast()) == p) {
                     stack.pollLast();
                     continue;
-                }
+                
             }
             stack.addLast(p);
         }
