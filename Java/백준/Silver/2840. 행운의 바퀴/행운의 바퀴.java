6e64480320f06aa7b032b1513aeb474bbbf9a4e7
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-14 11:51:15 +0900

    [Silver IV] Title: 행운의 바퀴, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ Java/백준/Silver/2840. 행운의 바퀴/행운의 바퀴.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        boolean failed = false;
+        char[] wheel = new char[N];
+        Map<Character, Integer> occurrences = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            wheel[i] = '?';
+        }
+        int idx = 0;
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int turn = Integer.parseInt(st.nextToken());
+            char letter = st.nextToken().charAt(0);
+            idx = (idx + N*100 - turn) % N;
+            // System.out.printf("turn후 idx: %d, turn: %d, letter: %s, 기존 letter: %s\n", idx, turn, letter, wheel[idx]);
+            if ((wheel[idx] != '?' && wheel[idx] != letter) || (wheel[idx] != letter && occurrences.get(letter) != null)) {
+               failed = true;
+            } else {
+                wheel[idx] = letter;
+                occurrences.put(letter, 1);
+            }
+        }
+
+        if (failed) System.out.append("!");
+        else System.out.append(resultToString(wheel, idx));
+        
+    }
+
+    static String resultToString(char[] wheel, int idx) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < wheel.length; i++) {
+            int printIdx = (idx+i) % wheel.length;
+            sb.append(wheel[printIdx]);
+        }
+        return sb.toString();
+    }
+}
