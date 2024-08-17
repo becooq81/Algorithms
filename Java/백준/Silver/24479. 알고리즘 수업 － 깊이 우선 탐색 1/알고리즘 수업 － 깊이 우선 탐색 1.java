dbe13e3ab52487da974d6f912bb2ed5e16586b7c
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-17 13:32:58 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 864 ms, Memory: 118172 KB -BaekjoonHub

--- Java/백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ Java/백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -2,42 +2,56 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] order;
-    static int idx = 1;
+
+    static int order = 1;
+    static int[] result;
+    static Map<Integer, TreeSet<Integer>> map;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
 
-        Map<Integer, TreeSet<Integer>> map = new HashMap<>();
+        map = new HashMap<>();
         for (int i = 1; i <= N; i++) {
-            map.put(i, new TreeSet<>());
+            map.put(i, new TreeSet<>((a, b) -> -Integer.compare(a, b)));
         }
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-
             map.get(x).add(y);
             map.get(y).add(x);
         }
+        result = new int[N+1];
+        dfs(R);
 
-        order = new int[N+1];
-        dfs(N, map, R);
-        for (int i = 1; i <= N; i++) {
-            System.out.println(order[i]);
+        for (int i = 1; i < N+1; i++) {
+            sb.append(result[i]).append("\n");
         }
-        
+        bw.write(sb.toString());
+        bw.flush();
         br.close();
     }
 
-    private static void dfs(int N, Map<Integer, TreeSet<Integer>> map, int node) {
-        order[node] = idx++;
-        for (int i : map.get(node)) {
-            if (order[i] == 0) dfs(N, map, i);
+    private static void dfs(int start) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        stack.push(start);
+
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
+            if (result[node] == 0) {
+                result[node] = order++;
+                for (int adjacent : map.get(node)) {
+                    stack.push(adjacent);
+                }
+            }
         }
     }
+    
 }
