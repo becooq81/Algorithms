c2d0fddf7bfaaed3e24c83f4865739dc42ad65ff
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-20 12:52:46 +0900

    [D4] Title: 의석이의 우뚝 선 산, Time: 697 ms, Memory: 103,464 KB -BaekjoonHub

---
+++ Java/SWEA/D4/4796. 의석이의 우뚝 선 산/의석이의 우뚝 선 산.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    static int[] mountains;
+    static int ans;
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int T = sc.nextInt();
+        for (int t = 0; t < T; t++) {            
+            ans = 0;
+            int N = sc.nextInt();
+            mountains = new int[N];
+
+            int idx = 0;
+            for (int i = 0; i < N; i++) {
+                mountains[idx++] = sc.nextInt();
+            }
+
+            boolean increasing = false;
+            boolean interval = false;
+            int increasingCount = 0;
+            for (int i = 1; i < N; i++) {
+                // System.out.print(i+": ");
+
+                if (mountains[i-1] < mountains[i]) {
+                    if (!increasing) increasingCount=0;
+                    increasing = true;
+                    interval = false;
+                    increasingCount++;
+                    // System.out.println(" 전거보다 큼");
+                } else {
+                    if (increasing) {
+                        increasing = false;
+                        interval = true;
+                        ans+= increasingCount;
+                        //increasingCount = 0;
+                        // System.out.println(" 커지는 중이였는데 작아짐");
+                    } else if (interval) {
+                        ans+= increasingCount;
+                        // System.out.println("  계속 작아지는중");
+                    }
+                }
+                // System.out.println("ANS: "+ans);
+            }
+
+            sb.append("#").append(t+1).append(" ").append(ans);
+            if (t != T-1) sb.append("\n");
+        }
+        System.out.append(sb);
+    }
+
+}
