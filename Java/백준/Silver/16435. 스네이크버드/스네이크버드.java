538e53447f186a5bb0a079881d3be8dc6db4feea
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-20 13:34:02 +0900

    [Silver V] Title: 스네이크버드, Time: 112 ms, Memory: 14440 KB -BaekjoonHub

---
+++ Java/백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] kg = {3, 5};
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int snake = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> fruitHeights = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            fruitHeights.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(fruitHeights);
+
+        while (!fruitHeights.isEmpty() && snake >= fruitHeights.get(0)) {
+            snake++;
+            fruitHeights.remove(0);
+        }
+
+        sb.append(snake);
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+    }
+}
