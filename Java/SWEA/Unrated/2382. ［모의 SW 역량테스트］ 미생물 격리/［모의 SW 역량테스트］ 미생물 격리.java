8c013ed9713eacb74d7e8fbaa7fb4f7843694939
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-27 17:18:07 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 746 ms, Memory: 115,656 KB -BaekjoonHub

--- Java/SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ Java/SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -8,10 +8,10 @@ public class Solution {
     static Organism[] organisms;
 
     static void simulate() {
-        for (int time = 1; time <=M; time++) {
-            Map<Key, List<Integer>> presence = new HashMap<>();
-            for (int i = 0; i < organisms.length; i++) { 
-                Organism o = organisms[i];
+        for (int time = 1; time <= M; time++) {
+            Map<Key, List<Organism>> presence = new HashMap<>();
+            
+            for (Organism o : organisms) {
                 o.y += DY[o.dir];
                 o.x += DX[o.dir];
                 if (isDrugged(o.y, o.x)) {
@@ -19,27 +19,30 @@ public class Solution {
                     o.dir = flipDirection(o.dir);
                 }
                 Key key = new Key(o.y, o.x);
-                if (!presence.containsKey(key)) presence.put(key, new ArrayList<>());
-                presence.get(key).add(i);
+                presence.computeIfAbsent(key, k -> new ArrayList<>()).add(o);
             }
 
-            for (Map.Entry<Key, List<Integer>> entry : presence.entrySet()) {
-                if (entry.getValue().size() >= 2) {
-                    int survivedIdx = -1;
-                    int maxCount = -1;
-                    int sumCount = 0;
-                    for (int idx : entry.getValue()) {
-                        if (organisms[idx].count > maxCount) {
-                            survivedIdx = idx;
-                            maxCount = organisms[idx].count;
-                        }
-                        sumCount += organisms[idx].count;
-                    }
-                    organisms[survivedIdx].count = sumCount;
+            for (List<Organism> list : presence.values()) {
+                if (list.size() > 1) {
+                    int maxCount = 0;
+                    Organism survivor = null;
+                    int totalCount = 0;
 
-                    for (int idx : entry.getValue()) {
-                        if (idx != survivedIdx) {
-                            organisms[idx].count = 0;
+                    for (Organism o : list) {
+                        if (o.count > maxCount) {
+                            maxCount = o.count;
+                            survivor = o;
+                        }
+                        totalCount += o.count;
+                    }
+
+                    if (survivor != null) {
+                        survivor.count = totalCount;
+                    }
+
+                    for (Organism o : list) {
+                        if (o != survivor) {
+                            o.count = 0;
                         }
                     }
                 }
@@ -49,32 +52,20 @@ public class Solution {
 
     static class Key {
         int y, x;
-        Key (int y, int x) {
+        Key(int y, int x) {
             this.y = y;
             this.x = x;
         }
         @Override
         public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + y;
-            result = prime * result + x;
-            return result;
+            return Objects.hash(y, x);
         }
         @Override
         public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
             Key other = (Key) obj;
-            if (y != other.y)
-                return false;
-            if (x != other.x)
-                return false;
-            return true;
+            return y == other.y && x == other.x;
         }
     }
 
@@ -83,12 +74,12 @@ public class Solution {
     }
 
     static boolean isDrugged(int y, int x) {
-        return y == 0 || x == 0 || y == N-1 || x == N-1;
+        return y == 0 || x == 0 || y == N - 1 || x == N - 1;
     }
 
     static class Organism {
-        int y, x, count, dir; 
-        Organism (int y, int x, int count, int dir) {
+        int y, x, count, dir;
+        Organism(int y, int x, int count, int dir) {
             this.y = y;
             this.x = x;
             this.count = count;
@@ -98,14 +89,10 @@ public class Solution {
 
     static int translateDir(int dir) {
         switch (dir) {
-            case 1: // 상
-                return 0;
-            case 2: // 하
-                return 2;
-            case 3: // 좌
-                return 3;
-            default: // 우
-                return 1;
+            case 1: return 0; // 상
+            case 2: return 2; // 하
+            case 3: return 3; // 좌
+            default: return 1; // 우
         }
     }
 
@@ -129,7 +116,7 @@ public class Solution {
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-            
+
             organisms = new Organism[K];
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -140,8 +127,7 @@ public class Solution {
                 organisms[i] = new Organism(y, x, count, dir);
             }
             simulate();
-            sb.append("#").append(t+1).append(" ").append(sumSurviving()).append("\n");
-
+            sb.append("#").append(t + 1).append(" ").append(sumSurviving()).append("\n");
         }
 
         bw.write(sb.toString());
@@ -149,5 +135,4 @@ public class Solution {
         bw.close();
         br.close();
     }
-
 }
