8ca56216235027181db6d49a5bac4878c5aa83b0
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-28 13:45:10 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 148 ms, Memory: 22,912 KB -BaekjoonHub

---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    
+    static int N, M, parents[], size[];
+
+    static void make() {
+        parents = new int[N+1];
+        size = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            parents[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    static int findSet(int a) {
+        if (parents[a] == a) return a;
+        return parents[a] = findSet(parents[a]);
+    }
+
+    static void union(int a, int b) {
+        int aRoot = findSet(a);
+        int bRoot = findSet(b);
+        if (aRoot == bRoot) return;
+        if (size[aRoot] < size[bRoot]) {
+            parents[aRoot] = bRoot;
+            size[bRoot] += size[aRoot];    
+        } else {
+            parents[bRoot] = aRoot;
+            size[aRoot] += size[bRoot];
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            make();
+            for (int m = 0; m < M; m++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                union(a, b);
+            }
+
+            Set<Integer> ans = new HashSet<>();
+            for (int i = 1; i <= N; i++) {
+                findSet(i);
+            }
+            for (int i = 1; i <= N; i++) {
+                ans.add(parents[i]);
+            }
+            sb.append("#").append(t + 1).append(" ").append(ans.size());
+            if (t != T - 1) sb.append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
