68262b370164fb598d956bd99be3f358c21819b6
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-30 11:57:15 +0900

    [D4] Title: 서로소 집합, Time: 464 ms, Memory: 109,360 KB -BaekjoonHub

--- Java/SWEA/D4/3289. 서로소 집합/서로소 집합.java
+++ Java/SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -1,10 +1,10 @@
 import java.util.*;
 import java.io.*;
- 
+  
 public class Solution {
-     
+      
     static int N, M, parents[], size[];
- 
+  
     static void make() {
         parents = new int[N + 1];
         size = new int[N + 1];
@@ -13,17 +13,17 @@ public class Solution {
             size[i] = 1;
         }
     }
- 
+  
     static int findSet(int a) {
         if (a == parents[a]) return a;
         return parents[a] = findSet(parents[a]);
     }
- 
+  
     static void union(int a, int b) {
         int aRoot = findSet(a);
         int bRoot = findSet(b);
         if (aRoot == bRoot) return;
-         
+          
         if (size[aRoot] < size[bRoot]) {
             parents[aRoot] = bRoot;
             size[bRoot] += size[aRoot];
@@ -32,23 +32,23 @@ public class Solution {
             size[aRoot] += size[bRoot];
         }
     }
- 
+  
     static int check(int a, int b) {
         return findSet(a) == findSet(b) ? 1 : 0;
     }
- 
+  
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
- 
+  
         int T = Integer.parseInt(st.nextToken());
         for (int t = 0; t < T; t++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
-             
+              
             StringBuilder ans = new StringBuilder();
             make();
             for (int m = 0; m < M; m++) {
@@ -56,11 +56,11 @@ public class Solution {
                 int cmd = Integer.parseInt(st.nextToken());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
-                 
+                  
                 if (cmd == 0) union(a, b);
                 else ans.append(check(a, b));
             }
- 
+  
             sb.append("#").append(t + 1).append(" ").append(ans);
             if (t != T - 1) sb.append("\n");
         }
