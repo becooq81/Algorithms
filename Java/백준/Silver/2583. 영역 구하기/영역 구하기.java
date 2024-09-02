535208f75a18f3c2484a27ac7f219d51d027985b
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-09-02 11:37:42 +0900

    [Silver I] Title: 영역 구하기, Time: 116 ms, Memory: 14432 KB -BaekjoonHub

--- Java/백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ Java/백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,91 +1,124 @@
 import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-    private static StringBuilder sb = new StringBuilder();
-    
-    static int m, n, k;
-    static int[][] grid;
+	static BufferedReader br;
+	static StringTokenizer st;
+	static BufferedWriter bw;
+	static StringBuilder sb;
+	static int M, N, K, grid[][];
+	static boolean visited[][];
+	static final int[] DY = {-1, 1, 0, 0}, DX = {0, 0, -1, 1};
+	
+	static int bfs(int startY, int startX) {
+		ArrayDeque<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[] {startX, startY});
+		visited[startY][startX] = true;
+		int count = 1;
+		
+		while (!queue.isEmpty()) {
+			int[] node = queue.pollFirst();
+			int x = node[0];
+			int y = node[1];
+			// System.out.println("Start at: "+y+","+x);
+			
+			for (int d = 0; d < 4; d++) {
+				int ny = y + DY[d];
+				int nx = x + DX[d];
+				// System.out.println("   Exploring: "+y+","+x);
+				
+				if (isValidCoordinate(nx, ny) && !visited[ny][nx] && grid[ny][nx] == 0) {
+					// System.out.println("         SUccess");
+					visited[ny][nx] = true;
+					count ++;
+					queue.add(new int[] {nx, ny});
+				}
+			}
+		}
+		return count;
+		
+	}
+	
+	static boolean isValidCoordinate(int startX, int startY) {
+		return startX >= 0 && startY >= 0 && startY < M && startX < N;
+	}
+	
+	
+	static void fill(int startX, int startY, int endX, int endY) {
+		for (int r = startY; r < endY; r++) {
+			for (int c = startX; c < endX; c++) {
+				grid[r][c] =1;
+			}
+		}
+		/*for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				System.out.print(grid[i][j]);
+			}
+			System.out.println();
+		}
+		
+		System.out.println("-------");*/
+	}
+	
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		grid = new int[M][N];
+		visited = new boolean[M+1][N+1];
+		for (int k = 0; k < K; k++) {
+			st = new StringTokenizer(br.readLine());
+			int startX = Integer.parseInt(st.nextToken());
+			int startY = Integer.parseInt(st.nextToken());
+			int endX = Integer.parseInt(st.nextToken());
+			int endY = Integer.parseInt(st.nextToken());
+			// System.out.println(startX+" "+startY+" " + endX + " " +endY);
 
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-
-    public static void main(String[] args) throws IOException {
-        String[] mnk = br.readLine().split(" ");
-        m = Integer.parseInt(mnk[0]);
-        n = Integer.parseInt(mnk[1]);
-        k = Integer.parseInt(mnk[2]);
-
-        int[][] rectangles = new int[k][4];
-        grid = new int[m][n];
-
-        for (int i = 0; i < k; i++) {
-            String[] input = br.readLine().split(" ");
-            for (int j = 0; j < 4; j++) {
-                rectangles[i][j] = Integer.parseInt(input[j]);
-            }
-        }
-
-        
-        for (int i = 0; i < k; i++) {
-            int leftBottomX = rectangles[i][0];
-            int leftBottomY = rectangles[i][1];
-            int rightTopX = rectangles[i][2]-1;
-            int rightTopY = rectangles[i][3]-1;
-
-            for (int x = leftBottomX; x <= rightTopX; x++) {
-                for (int y = leftBottomY; y <= rightTopY; y++) {
-                    grid[y][x] = 1;
-                }
-            }
-
-        }
-        int count = 0;
-
-        List<Integer> areas = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[j][i] == 0) {
-                    int area = bfs(i, j);
-                    count++;
-                    areas.add(area);
-                }
-            }
-        }
-        Collections.sort(areas);
-        sb.append(count).append("\n");
-        for (int area : areas) {
-            sb.append(area).append(" ");
-        }
-        System.out.append(sb);
-    }
-
-    private static int bfs(int startX, int startY) {
-        LinkedList<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {startX, startY});
-        grid[startY][startX] = 1;
-        int area = 1;
-        //System.out.printf("Start x: %d, start y: %d, area: %d\n", startX, startY, area);
-        while (!queue.isEmpty()) {
-            int[] node = queue.removeFirst();
-            int x = node[0];
-            int y = node[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m && grid[ny][nx] == 0) {
-                    
-                    grid[ny][nx] = 1;
-                    queue.add(new int[] {nx, ny});
-                    area++;
-
-                    //System.out.printf("     nx: %d, ny: %d, area: %d\n", nx, ny, area);
-                }
-            }
-        }
-        return area;
-    }
+			fill(startX, startY, endX, endY);
+		}
+		
+		/*for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(grid[i][j]);
+			}
+			System.out.println();
+		}*/
+		
+		int areaCount = 0;
+		List<Integer> areas = new ArrayList<>();
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				if (grid[i][j] == 0 && !visited[i][j]) {
+					// System.out.println("BFS at "+i+","+j);
+					areas.add(bfs(i, j));
+					areaCount++;
+				}
+			}
+		}
+		
+		Collections.sort(areas);
+		
+		sb.append(areaCount).append("\n");
+		for (int a : areas) {
+			sb.append(a).append(" ");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
