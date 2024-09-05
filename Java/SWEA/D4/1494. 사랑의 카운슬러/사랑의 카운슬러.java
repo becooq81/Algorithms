49685a07ca8d92f6c55241d820d7a4f9d2855ee7
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-09-05 10:27:12 +0900

    [D4] Title: 사랑의 카운슬러, Time: 356 ms, Memory: 21,068 KB -BaekjoonHub

--- Java/SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.java
+++ Java/SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.java
@@ -9,51 +9,28 @@ public class Solution {
 
     static int N, worms[][];
     static long ans;
-    static Set<Integer> visited = new HashSet<>();
+    static boolean[] visited;
 
-
-    static void check(int bitmask) {
-        int[] notChosen = new int[N/2];
-        int[] chosen = new int[N/2];
-        int idxChosen = 0, idxNotChosen = 0;
-        for (int i =0; i < N; i++) {
-            if ((bitmask & (1 << i)) != 0) chosen[idxChosen++] = i;
-            else notChosen[idxNotChosen++] = i;
-        }
-
-        long x = 0, y = 0;
-        for (int i = 0; i < N/2; i++) {
-            x += (worms[chosen[i]][0] - worms[notChosen[i]][0]);
-            y += worms[chosen[i]][1] - worms[notChosen[i]][1];
-        }
-        if (ans > y * y + x * x) {
-            ans = y * y + x * x;
-        }
-    }
-
-    static void genComb(int depth, int start, int bitmask) {
+    static void solve(int depth, int start) {
         if (depth == N/2) {
-            if (visited.contains(bitmask)) return;
-
-            check(bitmask);
-            
-            return;
+            long x = 0, y =0;
+            for (int i = 0; i < N; i++) {
+                if (visited[i]) {
+                    x += worms[i][0];
+                    y += worms[i][1];
+                }
+                else {
+                    x -= worms[i][0];
+                    y -= worms[i][1];
+                }
+            }
+            ans = Math.min(x * x + y * y, ans);
         }
-
         for (int i = start; i < N; i++) {
-            //if (selected[depth]) continue;
-            bitmask = bitmask | (1 << i);
-            genComb(depth+1, i+1, bitmask);
-            bitmask = bitmask & ~(1 << i);
+            visited[start] = true;
+            solve(depth + 1, i + 1);
+            visited[start] = false;
         }
-    }
-
-    static long getVectorMag(int[] w1, int[] w2) {
-        int y1 = w1[0];
-        int x1 = w1[1];
-        int y2 = w2[0];
-        int x2 = w2[1];
-        return (long) ((x2 - x1) * (x2 - x1)) + (long) ((y2 - y1) * (y2 - y1));
     }
 
     public static void main(String[] args) throws IOException {
@@ -68,16 +45,16 @@ public class Solution {
             N = Integer.parseInt(st.nextToken());
             ans = Long.MAX_VALUE;
             worms = new int[N][2];
-            // visited = new boolean[N];
+            visited = new boolean[N];
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 worms[i][0] = Integer.parseInt(st.nextToken());
                 worms[i][1] = Integer.parseInt(st.nextToken());
             }
-            for (int i = 0; i < N; i++) {
-                genComb(0, i, 0);
-            }
-                output.append("#").append(t).append(" ").append(ans);
+
+            solve(0, 0);
+            
+            output.append("#").append(t).append(" ").append(ans);
             if (t != T) output.append("\n");
         }
         bw.write(output.toString());
