7944b71c28d6e0ef5f8cd92e71f67620099c169b
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-09-09 10:25:05 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 145 ms, Memory: 20,696 KB -BaekjoonHub

--- Java/SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ Java/SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -2,71 +2,80 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    static BufferedReader br;
-    static BufferedWriter bw;
+
+    static int N, K, ans, nums[];
     static StringBuilder output;
-    static StringTokenizer st;
 
-    static int ans, swaps, numbers[];
-
-    static void solve(int depth, int start) {
-        if (depth == swaps) {
-            // System.out.println(Arrays.toString(numbers));
-            ans = Math.max(ans, getResult());
+    static void solve() {
+        while (K > nums.length) {
+            K -= 2;
         }
+        genComb(0, 0);
+    }
 
-        for (int i = start; i < numbers.length; i++) {
-            for (int j = 0; j < numbers.length; j++) {
-                if (i != j) {
-                    swap(i, j);
-                    solve(depth + 1, i+1);
-                    swap(i, j);
-                }
+    static void genComb(int depth, int start) {
+        if (depth == K) {
+            ans = Math.max(ans, getResult());
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) continue;
+                swap(i, j);
+                genComb(depth + 1, i +1);
+                swap(i, j);
             }
         }
     }
 
     static int getResult() {
-        int s = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            s += Math.pow(10, numbers.length-1-i) * numbers[i];
+        int tmp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            tmp += nums[i] * Math.pow(10, (nums.length - 1 - i));
         }
-        return s;
+        return tmp;
     }
 
     static void swap(int from, int to) {
-        int tmp = numbers[from];
-        numbers[from] = numbers[to];
-        numbers[to] = tmp;
+        int tmp = nums[from];
+        nums[from] = nums[to];
+        nums[to] = tmp;
     }
 
     public static void main(String[] args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        output = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
+        init();
+        output();
+    }
 
-        int T = Integer.parseInt(st.nextToken());
-        for (int t = 1; t <= T; t++) {
-            ans = 0;
-
-            st = new StringTokenizer(br.readLine());
-            char[] input = st.nextToken().toCharArray();
-            numbers = new int[input.length];
-            for (int i = 0; i < input.length; i++) {
-                numbers[i] = input[i] - '0';
-            }
-            swaps = Integer.parseInt(st.nextToken());
-            while (swaps >  numbers.length) {
-                swaps -=2;
-            }
-            solve(0, 0);
-            output.append("#").append(t).append(" ").append(ans);
-            if (t != T) output.append("\n");
-        }
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         bw.write(output.toString());
         bw.flush();
-        br.close();
         bw.close();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        output = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            ans = 0;
+            st = new StringTokenizer(br.readLine());
+            char[] input = st.nextToken().toCharArray();
+            nums = new int[input.length];
+            for (int i = 0; i < input.length; i++) {
+                nums[i] = input[i] - '0';
+            }
+
+            K = Integer.parseInt(st.nextToken());
+
+            solve();
+
+            output.append("#").append(t).append(" ").append(ans).append("\n");
+        }
+        br.close();
+    
     }
 }
