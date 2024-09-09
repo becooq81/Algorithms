d05f4a1851f841b912bb4ab5d1d9cc2545ad2948
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-09-09 09:29:21 +0900

    [D4] Title: 사랑의 카운슬러, Time: 320 ms, Memory: 20,760 KB -BaekjoonHub

--- Java/SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.java
+++ Java/SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.java
@@ -2,65 +2,70 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    static BufferedReader br;
-    static BufferedWriter bw;
+    
+    static int N;
+    static long ans, pairs[][];
     static StringBuilder output;
-    static StringTokenizer st;
-
-    static int N, worms[][];
-    static long ans;
-    static boolean[] visited;
-
-    static void solve(int depth, int start) {
-        if (depth == N/2) {
+    
+    private static void solve(int depth, boolean[] chosen, int start) {
+        if (N / 2 == depth) {
             long x = 0, y = 0;
             for (int i = 0; i < N; i++) {
-                if (visited[i]) {
-                    x += worms[i][0];
-                    y += worms[i][1];
-                }
-                else {
-                    x -= worms[i][0];
-                    y -= worms[i][1];
-                }
+                if (chosen[i]) {
+                    x += pairs[i][0];
+                    y += pairs[i][1];
+                } else {
+                    x -= pairs[i][0];
+                    y -= pairs[i][1];
+                } 
+
             }
-            ans = Math.min(x * x + y * y, ans);
+
+            ans = Math.min(ans, x * x + y * y);
             return;
         }
+
         for (int i = start; i < N; i++) {
-            visited[start] = true;
-            solve(depth + 1, i + 1);
-            visited[start] = false;
+            chosen[i] = true;
+            solve(depth + 1, chosen, i + 1);
+            chosen[i] = false;
         }
     }
 
     public static void main(String[] args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        output = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
+        init();
+        output();
+    }
 
-        int T = Integer.parseInt(st.nextToken());
-        for (int t = 1; t <= T; t++) {
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            ans = Long.MAX_VALUE;
-            worms = new int[N][2];
-            visited = new boolean[N];
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                worms[i][0] = Integer.parseInt(st.nextToken());
-                worms[i][1] = Integer.parseInt(st.nextToken());
-            }
-
-            solve(0, 0);
-            
-            output.append("#").append(t).append(" ").append(ans);
-            if (t != T) output.append("\n");
-        }
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         bw.write(output.toString());
         bw.flush();
-        br.close();
         bw.close();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        output = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            N = Integer.parseInt(br.readLine());
+            ans = Long.MAX_VALUE;
+
+            pairs = new long[N][2];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                pairs[i][0] = Long.parseLong(st.nextToken());
+                pairs[i][1] = Long.parseLong(st.nextToken());
+            }
+            solve(0, new boolean[N], 0);
+
+
+            output.append("#").append(t).append(" ").append(ans).append("\n");
+        }
+        br.close();
+    
     }
 }
