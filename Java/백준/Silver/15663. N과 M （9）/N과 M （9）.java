94c707c637ca095ef45d0096e8aa4e7c7051d3ae
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-09-09 17:09:30 +0900

    [Silver II] Title: N과 M (9), Time: 204 ms, Memory: 23984 KB -BaekjoonHub

--- Java/백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ Java/백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,47 +1,57 @@
 import java.util.*;
 import java.io.*;
-public class Main {
-    static int N, M, ans, nums[];
-    static StringBuilder output = new StringBuilder();
-    static HashSet<String> set = new HashSet<>();
-    
-    private static void genPerm(int depth, int[] res, boolean[] visited) {
-        if (depth == M) {
-            StringBuilder tmp = new StringBuilder();
-            for (int a : res) {
-                tmp.append(a).append(" ");
-            }
-            if (set.add(tmp.toString())) output.append(tmp.toString()).append("\n");
-            return;
-        }
 
-        for (int i = 0; i < N; i++) {
-            if (visited[i]) continue;
-            visited[i] = true;
-            res[depth] = nums[i];
-            genPerm(depth+1, res, visited);
-            visited[i] = false;
-        }
-    }
+public class Main {
+    static int n, m;
+    static int[] nums;
+    static boolean[] visited;
+    static List<Integer> route = new ArrayList<>();
+    static StringBuilder output = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        nums = new int[N];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        nums = new int[n];
+        visited = new boolean[n];
+        
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(nums);
-        genPerm(0, new int[M], new boolean[N]);
+        
+        Arrays.sort(nums); 
+        dfs(0);
 
         bw.write(output.toString());
         bw.flush();
-        br.close();
         bw.close();
+        br.close();
+    }
+    
+    public static void dfs(int cnt) {
+        if (route.size() == m) {
+            for (int a : route) {
+                output.append(a).append(" ");
+            }
+            output.append("\n");
+            return;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (visited[i] || (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])) {
+                continue;
+            }
+            
+            route.add(nums[i]);
+            visited[i] = true;
+            dfs(cnt + 1);
+            route.remove(route.size() - 1);
+            visited[i] = false;
+        }
     }
 }
