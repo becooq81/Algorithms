1de710e2cf341c61fc0522af2b30508436d28113
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-09-09 09:40:08 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 156 ms, Memory: 25,692 KB -BaekjoonHub

--- Java/SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ Java/SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,68 +1,82 @@
 import java.util.*;
 import java.io.*;
- 
+
 public class Solution {
-     
-    static int N, M, parents[], size[];
- 
+    
+    static int N, M, ans, parents[], size[];
+    static StringBuilder output;
+    
+
     static void make() {
         parents = new int[N+1];
         size = new int[N+1];
-        for (int i = 1; i <= N; i++) {
-            parents[i] = i;
-            size[i] = 1;
-        }
+        Arrays.fill(parents, -1);
+        Arrays.fill(size, 1);
     }
- 
+
     static int findSet(int a) {
-        if (parents[a] == a) return a;
+        if (parents[a] < 0) return a;
         return parents[a] = findSet(parents[a]);
     }
- 
-    static void union(int a, int b) {
+
+    static boolean union(int a, int b) {
         int aRoot = findSet(a);
         int bRoot = findSet(b);
-        if (aRoot == bRoot) return;
-        if (size[aRoot] < size[bRoot]) {
+        if (aRoot == bRoot) return false;
+        if (size[bRoot] > size[aRoot]) {
+            size[bRoot] += size[aRoot];
             parents[aRoot] = bRoot;
-            size[bRoot] += size[aRoot];    
         } else {
-            parents[bRoot] = aRoot;
             size[aRoot] += size[bRoot];
+            parents[bRoot] = aRoot;
         }
+        return true;
     }
- 
- 
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        init();
+        output();
+    }
+
+    private static void output() throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        int T = Integer.parseInt(st.nextToken());
-        for (int t = 0; t < T; t++) {
+        bw.write(output.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        output = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
+
             make();
-            for (int m = 0; m < M; m++) {
+            for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
- 
                 union(a, b);
             }
- 
-            Set<Integer> ans = new HashSet<>();
+
             for (int i = 1; i <= N; i++) {
-                ans.add(findSet(i));
+                findSet(i);
             }
-            sb.append("#").append(t + 1).append(" ").append(ans.size());
-            if (t != T - 1) sb.append("\n");
+
+            Set<Integer> crowds = new HashSet<>();
+            for (int i = 1; i <= N; i++) {
+                crowds.add(parents[i] == -1 ? i : parents[i]);
+            }
+
+
+            output.append("#").append(t).append(" ").append(crowds.size()).append("\n");
         }
-        bw.write(sb.toString());
-        bw.flush();
         br.close();
-        bw.close();
+    
     }
 }
