fe607658a8a8c6b9a9b8631f81dad53c2f5f88d0
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-09-17 22:29:28 +0900

    [Gold V] Title: 상어 초등학교, Time: 220 ms, Memory: 22432 KB -BaekjoonHub

--- Java/백준/Gold/21608. 상어 초등학교/상어 초등학교.java
+++ Java/백준/Gold/21608. 상어 초등학교/상어 초등학교.java
@@ -104,7 +104,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder output = new StringBuilder();
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
