f795e2aa8e9822ef1212409d60584f36fc1ede2a
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-10-02 14:02:06 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 112 ms, Memory: 18,384 KB -BaekjoonHub

--- Java/SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ Java/SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -1,8 +1,8 @@
 import java.util.*;
 import java.io.*;
-
+ 
 public class Solution {
-
+ 
     public static void rotate(int[][] magnets, int index, int direction) {
         if (direction == 1) { 
             int temp = magnets[index][7];
@@ -18,11 +18,11 @@ public class Solution {
             magnets[index][7] = temp;
         }
     }
-
+ 
     public static void propagateRotations(int[][] magnets, int index, int direction) {
         int[] rotations = new int[4];
         rotations[index] = direction;
-
+ 
         for (int i = index; i > 0; i--) {
             if (magnets[i][6] != magnets[i - 1][2]) { 
                 rotations[i - 1] = -rotations[i]; 
@@ -30,7 +30,7 @@ public class Solution {
                 break;
             }
         }
-
+ 
         for (int i = index; i < 3; i++) {
             if (magnets[i][2] != magnets[i + 1][6]) {
                 rotations[i + 1] = -rotations[i]; 
@@ -38,56 +38,56 @@ public class Solution {
                 break;
             }
         }
-
+ 
         for (int i = 0; i < 4; i++) {
             if (rotations[i] != 0) {
                 rotate(magnets, i, rotations[i]);
             }
         }
     }
-
+ 
     public static int calculateScore(int[][] magnets) {
         int score = 0;
         int[] scores = {1, 2, 4, 8}; 
-
+ 
         for (int i = 0; i < 4; i++) {
             if (magnets[i][0] == 1) {
                 score += scores[i];
             }
         }
-
+ 
         return score;
     }
-
+ 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder output = new StringBuilder();
-
-        int T = Integer.parseInt(reader.readLine());
-
+        StringTokenizer st;
+ 
+        int T = Integer.parseInt(br.readLine());
+ 
         for (int t = 1; t <= T; t++) {
-            int K = Integer.parseInt(reader.readLine());
+            int K = Integer.parseInt(br.readLine());
             int[][] magnets = new int[4][8];
-
+ 
             for (int i = 0; i < 4; i++) {
-                String[] inputs = reader.readLine().split(" ");
+                st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < 8; j++) {
-                    magnets[i][j] = Integer.parseInt(inputs[j]);
+                    magnets[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-
+ 
             for (int i = 0; i < K; i++) {
-                String[] rotationInfo = reader.readLine().split(" ");
-                int index = Integer.parseInt(rotationInfo[0]) - 1;
-                int direction = Integer.parseInt(rotationInfo[1]); 
+                st = new StringTokenizer(br.readLine());
+                int index = Integer.parseInt(st.nextToken()) - 1;
+                int direction = Integer.parseInt(st.nextToken()); 
                 propagateRotations(magnets, index, direction);
             }
-
+ 
             int score = calculateScore(magnets);
             output.append("#").append(t).append(" ").append(score).append("\n");
         }
-
-        System.out.print(output);
-        reader.close();
+ 
+        System.out.println(output);
     }
 }
