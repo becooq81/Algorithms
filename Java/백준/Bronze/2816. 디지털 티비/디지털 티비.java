2fe557c2ad7c63c702365fd141e49b387c393bfe
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-10-04 22:43:51 +0900

    [Bronze I] Title: 디지털 티비, Time: 100 ms, Memory: 14056 KB -BaekjoonHub

--- Java/백준/Bronze/2816. 디지털 티비/디지털 티비.java
+++ Java/백준/Bronze/2816. 디지털 티비/디지털 티비.java
@@ -1,7 +1,7 @@
 import java.io.*;
 public class Main {
     private static final String FIRST = "KBS1", SEC = "KBS2";
-    private static final int CURSOR_DOWN = 1, CURSOR_UP = 2, SWAP_DOWN = 3, SWAP_UP = 4;
+    private static final String CURSOR_DOWN = "1", SWAP_UP = "4";
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -14,11 +14,11 @@ public class Main {
         }
 
         if (kbs1_idx > kbs2_idx) kbs2_idx++;
-        for (int i = 0; i < kbs1_idx; i++) output.append(CURSOR_DOWN);
-        for (int i =0; i < kbs1_idx; i++) output.append(SWAP_UP);
+        output.append(CURSOR_DOWN.repeat(kbs1_idx));
+        output.append(SWAP_UP.repeat(kbs1_idx));
         if (kbs2_idx != 1) {
-            for (int i = 0; i < kbs2_idx; i++) output.append(CURSOR_DOWN);
-            for (int i =1; i < kbs2_idx; i++) output.append(SWAP_UP);
+            output.append(CURSOR_DOWN.repeat(kbs2_idx));
+            output.append(SWAP_UP.repeat(kbs2_idx-1));
         }
         System.out.append(output);
     }
