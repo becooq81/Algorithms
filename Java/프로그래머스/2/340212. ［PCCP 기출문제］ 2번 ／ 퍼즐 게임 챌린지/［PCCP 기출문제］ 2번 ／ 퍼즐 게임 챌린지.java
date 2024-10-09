e753fdd4311baebc961119005f3ff6b950518625
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-10-09 12:59:51 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 29.64 ms, Memory: 131 MB -BaekjoonHub

--- Java/프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ Java/프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -18,23 +18,13 @@ class Solution {
     
     private boolean check(int mid, int[] diffs, int[] times, long limit) {
         long timeTaken = times[0];
-        //System.out.print("Mid: "+mid+": ");
-
         for (int i = 1; i < diffs.length; i++) {
             if (diffs[i] <= mid) {
                 timeTaken += times[i];
-                // System.out.println ("solved("+times[i]+"), ");
             } else {
-                timeTaken += (diffs[i] - mid) * (times[i] + times[i-1]) + times[i];
-                // System.out.print("Notsolved("+times[i] * (diffs[i] - mid + 1)+"),");
-                
-                // timeTaken += (long) times[i-1];
-                
-                // System.out.println();
-                
+                timeTaken += (diffs[i] - mid) * (times[i] + times[i-1]) + times[i];             
             }
         }
-        // System.out.println("total: "+timeTaken);
         return limit >= timeTaken;
     }
 }
